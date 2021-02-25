8ddcde08124626c75e71e99b23634c465c526354
brianpompey <brianthescholar@gmail.com>
2021-02-25 12:03:42 -0500

    coded out the attr controller will need edits im sure

--- app/controllers/profile_attrs_controller.rb
+++ app/controllers/profile_attrs_controller.rb
@@ -1,13 +1,29 @@
 class ProfileAttrsController < ApplicationController
   def new
+    @user = current_user
   end
 
   def create
+    current_user.create_profile_attr(profile_attr_params)
+    redirect_to user_path(current_user)
   end
 
   def edit
+    @user = current_user
+    @profile_attr = current_user.profile_attr
   end
 
   def update
+    if current_user.profile_attr.update(profile_attr_params)
+      redirect_to user_path(current_user)
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def profile_attr_params
+    params.require(:profile_attr).permit(:name, :about, :location, :company, :education, :user_id, :image)
   end
 end
