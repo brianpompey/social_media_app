8235c4e66b6157c14285a03d172c2588a4e10730
brianpompey <brianthescholar@gmail.com>
2021-02-25 12:08:35 -0500

    added the last model tweet so i have everything in front of me

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_25_164251) do
+ActiveRecord::Schema.define(version: 2021_02_25_170744) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -45,6 +45,14 @@ ActiveRecord::Schema.define(version: 2021_02_25_164251) do
     t.index ["user_id"], name: "index_profile_attrs_on_user_id"
   end
 
+  create_table "tweets", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_tweets_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
@@ -56,4 +64,5 @@ ActiveRecord::Schema.define(version: 2021_02_25_164251) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "profile_attrs", "users"
+  add_foreign_key "tweets", "users"
 end
