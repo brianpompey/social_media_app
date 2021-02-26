890d33f2de6ce33338e2cc5491cfc50a02f3a73e
brianpompey <brianthescholar@gmail.com>
2021-02-26 16:44:01 -0500

    Adding to the controllers

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -22,9 +22,15 @@ class TweetsController < ApplicationController
   end
 
   def edit
+    @tweet = Tweet.find(params[:id])
   end
 
   def update
+    if @tweet.update(tweet_params)
+      redirect_to root_path
+    else
+      render 'edit'
+    end
   end
 
   def destroy
