c0a233042f7376be8be1dc80fb3c1ba572acee09
brianpompey <brianthescholar@gmail.com>
2021-02-25 20:54:38 -0500

    user has many tweets

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   has_one :profile_attr
+  has_many :tweets
 
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
