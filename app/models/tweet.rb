f6f869a5233672b245ceba6133841c282dd1be42
brianpompey <brianthescholar@gmail.com>
2021-02-25 21:25:09 -0500

    another tweet validation

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,5 +2,5 @@ class Tweet < ApplicationRecord
   belongs_to :user
 
   validates :content, presence: true
-  validates :content, length: { maximum: 280 }
+  validates :content, length: { maximum: 280, too_long: "%{count} characters is the maximum allowed" }
 end
